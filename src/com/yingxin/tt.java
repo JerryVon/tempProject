ff2c9b4854a44035aa4b3cae65490ee449acfbc6
xuwang <xuwang@yingxingroup.net>
2018-03-14 10:43:02 +0800

    test

---
+++ src/com/yingxin/tt.java
@@ -0,0 +1,15 @@
+package com.yingxin;
+
+/**
+ * tt
+ *
+ * @author NiuHeng
+ * @date 2018/3/14 10:01
+ */
+public class tt {
+
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+
+}
